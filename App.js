20a00a2f7980384596fbc28a1d3aceffd261a0a7
Emmanuel Yidana <100804353+Emmanuelitgit@users.noreply.github.com>
2023-11-28 20:51:12 +0000

    front-end

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from "./src/Components/Navigation"
+import { useState } from 'react';
 
 
 
