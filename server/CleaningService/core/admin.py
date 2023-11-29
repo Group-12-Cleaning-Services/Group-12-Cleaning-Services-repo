20f3be3de1ac017db2dacd777d37c01fc501999e
Emmanuel Yidana <100804353+Emmanuelitgit@users.noreply.github.com>
2023-11-29 18:21:28 +0000

    front-end

--- server/CleaningService/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from core.models import *
-# Register your models here.
-admin.site.register(CleaningServiceUser)
-admin.site.register(VerificationToken)
-admin.site.register(PasswordToken)
-admin.site.register(CleaningServiceUserProfile)
-admin.site.register(Service)
